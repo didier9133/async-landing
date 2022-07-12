516f3a3fc935314c068147f4e2c97771e7213146
Didier Contreras <didier9133@gmail.com>
2022-07-12 14:02:55 -0400

    cambios en el numero de videos

--- src/assets/main.js
+++ src/assets/main.js
@@ -34,7 +34,7 @@ async function fetchData(urlApi){
                         </h3>
                     </div>
                 </div>
-            `).slice(0,4).join("")}
+            `)}
             
         `;
         
